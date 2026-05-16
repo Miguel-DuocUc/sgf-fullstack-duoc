1e3cc0fb209ded6a838f69263f564868b030719c
seba259 <sebastianmonsalve2848@gmail.com>
2026-05-15 23:25:54 -0400

    creación de DTOs y cliente Feign para validación de usuarios
    Se agregan DTOs de entrada y salida para separar los datos recibidos y enviados por la API del modelo persistente IdentityDocument. También se incorpora UserBasicDto y UserClient usando OpenFeign para permitir la comunicación con MS-Users, validando información remota del usuario antes de gestionar documentos de identidad.

--- ms-identity/src/main/java/com/duoc/sgf/ms_identity/client/UserClient.java
+++ ms-identity/src/main/java/com/duoc/sgf/ms_identity/client/UserClient.java
@@ -1,4 +1,13 @@
 package com.duoc.sgf.ms_identity.client;
 
-public class UserClient {
+import com.duoc.sgf.ms_identity.dto.UserBasicDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "ms-users", url = "${clients.ms-users.url}")
+public interface UserClient {
+
+    @GetMapping("/api/v1/users/{id}")
+    UserBasicDto findById(@PathVariable("id") Long id);
 }
