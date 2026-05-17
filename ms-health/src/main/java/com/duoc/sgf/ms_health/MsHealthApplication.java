c31ce38cde3292ed47d43086c8191360a62a6f80
seba259 <sebastianmonsalve2848@gmail.com>
2026-05-16 20:19:42 -0400

    configuración de clientes Feign para comunicación con microservicios
    Se configuran los clientes OpenFeign del microservicio MS-Health para comunicarse con MS-Users y MS-Identity. Esta integración permite validar remotamente que el usuario exista, esté activo y posea un documento de identidad válido antes de registrar declaraciones sanitarias dentro del sistema aduanero.

--- ms-health/src/main/java/com/duoc/sgf/ms_health/MsHealthApplication.java
+++ ms-health/src/main/java/com/duoc/sgf/ms_health/MsHealthApplication.java
@@ -2,12 +2,13 @@ package com.duoc.sgf.ms_health;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.duoc.sgf.ms_health.client")
 public class MsHealthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsHealthApplication.class, args);
 	}
-
 }
