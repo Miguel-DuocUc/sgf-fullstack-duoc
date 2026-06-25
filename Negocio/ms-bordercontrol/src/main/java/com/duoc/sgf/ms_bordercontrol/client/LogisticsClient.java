ef7cf1aa9559c160677541347c00bddaa4187e6d
seba259 <sebastianmonsalve2848@gmail.com>
2026-06-24 21:18:22 -0400

    Integración de DTO y cliente Feign para validar puestos fronterizos desde ms-logistics

--- Negocio/ms-bordercontrol/src/main/java/com/duoc/sgf/ms_bordercontrol/client/LogisticsClient.java
+++ Negocio/ms-bordercontrol/src/main/java/com/duoc/sgf/ms_bordercontrol/client/LogisticsClient.java
@@ -1,12 +1,15 @@
 package com.duoc.sgf.ms_bordercontrol.client;
 
 
+import com.duoc.sgf.ms_bordercontrol.model.dto.LogisticsCheckpointBasicDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "ms-logistics")
 public interface LogisticsClient {
+
     @GetMapping("/api/v1/logistics/{id}")
-    Object obtenerPasoFronterizoPorId(@PathVariable("id") Long id);
+    LogisticsCheckpointBasicDto findById(@PathVariable Long id);
+
 }
